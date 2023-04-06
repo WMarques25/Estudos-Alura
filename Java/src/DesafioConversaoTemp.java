010c6935f40de6cbd9dd87ce02531b6e828055ae
Wellington Pereira Marques <ti.wmarques@outlook.com>
2023-04-06 10:35:16 -0300

    Initial commit

---
+++ Java/src/DesafioConversaoTemp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DesafioConversaoTemp {
+    public static void main(String[] args) throws Exception {
+        // Variaveis
+        float tc;
+        double tf;
+        Scanner sc = new Scanner(System.in);
+        
+        // Entrada + Calculo
+        System.out.print("Insira um valor: ");
+        tc = sc.nextFloat();
+        tf = (tc * 1.8) + 32;
+
+        // Saida
+        System.out.println(String.format("%.2fºC = %.2fºF", tc,tf));
+        sc.close();
+
+    }
+}
