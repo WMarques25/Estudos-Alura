a22324831a1ea2b78caa2687d91d618784bc5e87
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2023-05-08 12:48:50 -0300

    Teste de Login

---
+++ Selenium/2019-selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,17 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldSelenium {
+
+    @Test
+    public void hello() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/welli/Desktop/GitHub Repositorys/Estudos-Alura/Selenium/2019-selenium-java-projeto_inicial/src/Drivers/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("http://localhost:8080/leiloes");
+        browser.quit();
+    }
+    
+}
