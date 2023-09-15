763e5c9882eed00d385cbf1a2d695dc82afa3036
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2023-09-15 17:56:15 -0300

    Kotlin AluGames

---
+++ Kotlin/alugames/src/main/kotlin/Jogo.kt
@@ -0,0 +1,10 @@
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Titulo: '$titulo' \nCapa: '$capa'\nDescricao: '$descricao'"
+    }
+}
---
+++ Kotlin/alugames/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+    println(json)
+
+    val meuJogo = Jogo()
+     meuJogo.titulo = "The Witcher 3: Wild Hunt"
+     meuJogo.capa = "https://steamcdn-a.akamaihd.net/steam/apps/292030/header.jpg?t=1607526722"
+
+    println(meuJogo)
+}
