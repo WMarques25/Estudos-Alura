5cde1926e47ea4513c4319ee3ae122139e41d578
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2023-10-02 17:02:27 -0300

    Kotlin AluGames packeges

--- Kotlin/alugames/src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ Kotlin/alugames/src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -1,3 +1,8 @@
+package br.com.alura.alugames.principal
+
+import br.com.alura.alugames.modelo.InfoJogo
+import br.com.alura.alugames.modelo.Jogo
+import br.com.alura.alugames.servicos.ConsumoApi
 import com.google.gson.Gson
 import java.net.URI
 import java.net.http.HttpClient
@@ -10,34 +15,21 @@ fun main() {
     print("Digite o codigo do jogo: ")
     val busca = leitura.nextLine()
 
-    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
-
-    val client: HttpClient = HttpClient.newHttpClient()
-    val request = HttpRequest.newBuilder()
-        .uri(URI.create(endereco))
-        .build()
-    val response = client
-        .send(request, BodyHandlers.ofString())
-
-    val json = response.body()
-    println(json)
-
-    val gson = Gson()
-    val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+    val meuInfoJogo = ConsumoApi().buscaJogo(busca)
 
 //    try {
-//        val meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
+//        val meuJogo = br.com.alura.alugames.modelo.Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
 //        println(meuJogo)
 //    } catch (ex: NullPointerException){
-//        println("Jogo não encontrado, tente outro código.")
+//        println("br.com.alura.alugames.modelo.Jogo não encontrado, tente outro código.")
 //    }
-    var meuJogo:Jogo? = null
+    var meuJogo: Jogo? = null
 
     val resultado = runCatching {
         meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
     }
     resultado.onFailure {
-        println("Jogo não encontrado, tente outro código.")
+        println("br.com.alura.alugames.modelo.Jogo não encontrado, tente outro código.")
     }
     resultado.onSuccess {
         println("Deseja imserir uma descrição? (S/N)")
@@ -49,5 +41,9 @@ fun main() {
             meuJogo?.descricao = meuJogo?.titulo
         }
         println(meuJogo)
+    }
+
+    resultado.onSuccess {
+        println("Busca realizada com sucesso!")
     }
 }
