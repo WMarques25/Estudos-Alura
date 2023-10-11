8f9b8ad8a3c7501e93040c91d05a7d223e3f9624
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2023-10-11 12:11:07 -0300

    login.feature

--- bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/e2e/pages/LeiloesPage.java
+++ bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/e2e/pages/LeiloesPage.java
@@ -38,6 +38,16 @@ public class LeiloesPage {
 		return new NovoLeilaoPage(driver);
 	}
 
+	public boolean estaNaPaginaDeLeiloes() {
+		esperaCarregarPaginaDeLeiloes();
+		return this.driver.getCurrentUrl().endsWith("/leiloes");
+	}
+
+	public void esperaCarregarPaginaDeLeiloes() {
+		WebDriverWait wait = new WebDriverWait(driver, 60);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(),'Todos leilões')]")));
+	}
+
 	public DetalhesDoLeilaoPage visitaLeilaoPaginaParaDarLance() {
 		driver.findElement(By.linkText("dar lance")).click();
 		return new DetalhesDoLeilaoPage(driver);
