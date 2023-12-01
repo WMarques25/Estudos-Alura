21f5d6fff23f1e8476f002c84e1f74c69d98b267
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2023-12-01 12:45:17 -0300

    Criteria api

---
+++ Java/src/Java e Banco de Dados/JPA - Hibernate/loja/src/main/java/br/com/alura/testes/TesteCriteria.java
@@ -0,0 +1,49 @@
+package br.com.alura.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.dao.CategoriaDAO;
+import br.com.alura.dao.ProdutoDAO;
+import br.com.alura.modelo.Categoria;
+import br.com.alura.modelo.Produto;
+import br.com.alura.util.JPAUtil;
+
+public class TesteCriteria {
+    
+    public static void main(String[] args) {
+        popularBancoDeDados();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        produtoDAO.buscarPorParametrosComCriteria("PS5", null, null);
+        
+    }
+
+    private static void popularBancoDeDados() {
+        Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+        
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+        Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("4000"), videogames);
+        Produto macbook = new Produto("MacBook", "Macbook pro retina", new BigDecimal("14000"), informatica);
+        
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+
+        em.getTransaction().begin();
+
+        categoriaDAO.cadastrar(celulares);
+        categoriaDAO.cadastrar(videogames);
+        categoriaDAO.cadastrar(informatica);
+
+        produtoDAO.cadastrar(celular);
+        produtoDAO.cadastrar(videogame);
+        produtoDAO.cadastrar(macbook);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
