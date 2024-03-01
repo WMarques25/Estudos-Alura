4231518ded3f2e8946aa1e2b2921637f057048ab
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2024-03-01 10:10:30 -0300

    Security

--- Java/src/api/src/main/java/med/voll/api/controller/PacienteControler.java
+++ Java/src/api/src/main/java/med/voll/api/controller/PacienteControler.java
@@ -16,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.paciente.*;
 
 @RestController
 @RequestMapping("/pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteControler {
 
     @Autowired
