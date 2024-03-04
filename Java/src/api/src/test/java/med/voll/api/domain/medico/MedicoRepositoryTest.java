d1338b2f871d8018bb51faf4efe88b01dac01a7d
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2024-03-04 10:49:05 -0300

    Ambiente de tests

---
+++ Java/src/api/src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.medico;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class MedicoRepositoryTest {
+
+    @Test
+    public void testEscolherMedicoAleatorioLivreNaData() {
+        
+    }
+}
