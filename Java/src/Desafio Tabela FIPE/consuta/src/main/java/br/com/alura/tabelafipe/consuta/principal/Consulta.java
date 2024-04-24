4c601c83f0068cb1d10b425483cbca3d5f0ed83f
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2024-04-24 12:49:28 -0300

    a

--- Java/src/Desafio Tabela FIPE/consuta/src/main/java/br/com/alura/tabelafipe/consuta/principal/Consulta.java
+++ Java/src/Desafio Tabela FIPE/consuta/src/main/java/br/com/alura/tabelafipe/consuta/principal/Consulta.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 import br.com.alura.tabelafipe.consuta.service.ConsumoAPI;
 
 public class Consulta {
-    private Scanner scanner;
-    private ConsumoAPI consumoAPI;
+    private static Scanner scan = new Scanner(System.in);
+    private static ConsumoAPI consumoAPI = new ConsumoAPI();
 
     public static void consultar() {
-        
+        System.out.print("""
+                Qual o tipo de veiculo para consultar?
+                    1 - Motos
+                    2 - Carros
+                    3 - Caminhões
+                """);
+        consumoAPI.obterMarcas(scan.nextInt());
     }
 
 }
--- Java/src/Desafio Tabela FIPE/consuta/src/main/java/br/com/alura/tabelafipe/consuta/service/ConsumoAPI.java
+++ Java/src/Desafio Tabela FIPE/consuta/src/main/java/br/com/alura/tabelafipe/consuta/service/ConsumoAPI.java
@@ -1,6 +1,49 @@
 package br.com.alura.tabelafipe.consuta.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsumoAPI {
     private final String URL = "https://parallelum.com.br/fipe/api/v1";
+    private String tipoVeiculo;
     
+    public String obterMarcas(Integer i){
+        tipoVeiculo  = obterTipo(i) + "/marcas";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(URL + tipoVeiculo))
+                    .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+
+    public String obterTipo(Integer i){
+        switch(i){
+            case 1:
+            return "motos";
+
+            case 2:
+            return "carros";
+
+            case 3:
+            return "caminhoes";
+
+            default:
+            return null;
+        }
+    }
 }
