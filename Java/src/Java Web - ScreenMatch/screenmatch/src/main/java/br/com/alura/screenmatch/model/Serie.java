e6ec8a9621885f39ba40e8e63bbd141099aff2f5
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2024-07-01 13:15:30 -0300

    att

--- Java/src/Java Web - ScreenMatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ Java/src/Java Web - ScreenMatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -20,4 +20,72 @@ public class Serie {
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTempordas() {
+        return totalTempordas;
+    }
+
+    public void setTotalTempordas(Integer totalTempordas) {
+        this.totalTempordas = totalTempordas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+
+    @Override
+    public String toString() {
+        return "genero=" + genero +
+                ", titulo='" + titulo + '\'' +
+                ", totalTempordas=" + totalTempordas +
+                ", avaliacao=" + avaliacao +
+                ", atores='" + atores + '\'' +
+                ", poster='" + poster + '\'' +
+                ", sinopse='" + sinopse + '\'';
+    }
 }
--- Java/src/Java Web - ScreenMatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Java/src/Java Web - ScreenMatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,11 +1,14 @@
 package br.com.alura.screenmatch.principal;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 
@@ -70,6 +73,12 @@ public class Principal {
     }
 
     private void listarSeriesBuscadas() {
-        dadosSeries.forEach(System.out::println);
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream()
+                    .map(d -> new Serie(d))
+                    .collect(Collectors.toList());
+        series.stream()
+            .sorted(Comparator.comparing(Serie::getGenero))
+            .forEach(System.out::println);
     }
 }
